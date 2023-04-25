21ec38c3ba806648c94ea467e8ce0db158e862fe
Harriet Oteng <harriet.oteng@gmail.com>
2023-04-25 16:13:55 +0000

    Add comments and fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DigitButton from './DigitButton';
 
+// DigitButton component with prop validation
+function DigitButton({ digit }) {
+  return (
+    <button type="button" className="digit-button">{ digit }</button>
+  );
+}
+DigitButton.propTypes = {
+  digit: PropTypes.oneOfType(PropTypes.string, PropTypes.number).isRequired,
+};
+
+// OpertorButton component with prop validation
 function OperatorButton({ operator }) {
   return (
     <button type="button" className="operator-button">{ operator }</button>
   );
 }
-
 OperatorButton.propTypes = {
   operator: PropTypes.string.isRequired,
-  // digit: PropTypes.oneOfType(string, number).isRequired,
 };
 
+// Calculator App
 function Calculator() {
   const Operators = ['/', 'x', '-', '+', '='];
-  const Digits = ['AC', '+ / -', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
+  const Digits = ['AC', '+ / -', '%', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
 
+  // Create button elements from Operators array using OperatorButton component
   const operatorList = Operators.map((operator) => (
     <OperatorButton key={operator} operator={operator} />
   ));
 
+  // Create button elements from Digits array using DigitButton component
   const digitList = Digits.map((digit) => (
     <DigitButton key={digit} digit={digit} />
   ));
