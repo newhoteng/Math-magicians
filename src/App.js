c33f83c1b5b2480dcfaa39ee697b83e40b934353
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-02 22:53:46 +0000

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
 import './App.css';
+// App.js Link
+
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Math Magicians</li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/calculator">Calculator</a></li>
+          <li><a href="/quote">Quote</a></li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quote />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<HomePage />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+          <Route path="*" element={<div>Page not found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
