a6288bee80ebfdb5026a827510b231a1fc6d5b35
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-03 08:42:46 +0000

    Move navbar to its own component and add router basename

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import {
-  BrowserRouter, Routes, Route, Outlet, Link,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
 import HomePage from './components/HomePage';
+import NavBar from './components/NavBar';
 import './App.css';
-
-function NavBar() {
-  return (
-    <>
-      <nav className="nav">
-        <div>Math Magicians</div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="calculator">Calculator</Link></li>
-          <li><Link to="quote">Quote</Link></li>
-        </ul>
-      </nav>
-      <Outlet />
-    </>
-  );
-}
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Math-magicians">
       <Routes>
         <Route path="/" element={<NavBar />}>
           <Route index element={<HomePage />} />
-          <Route path="calculator" element={<Calculator />} />
-          <Route path="quote" element={<Quote />} />
-          <Route path="*" element={<div>Page not found</div>} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/*" element={<div>Page not found</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
